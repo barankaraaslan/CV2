f7ed21b8c677caabc6046d96a61215e204571bf3
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-10-29 18:27:05 +0300

    store cv in bucket

--- lib/cv2-stack.ts
+++ lib/cv2-stack.ts
@@ -1,8 +1,29 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { Asset } from "aws-cdk-lib/aws-s3-assets";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
+import { join } from "path";
 
 export class Cv2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const bucket = new Bucket(this, "cv-bucket", {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      bucketName: "cv-repo",
+      enforceSSL: true,
+    });
+
+    new BucketDeployment(this, "cv-deployment", {
+      sources: [
+        Source.asset(join(__dirname, "../"), {
+          exclude: ["**", "!cv.html"],
+        }),
+      ],
+      destinationBucket: bucket,
+    });
   }
 }
