687259f8d31052002e3e3121d70fa04a547fdc5d
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-10-30 17:03:03 +0300

    try to use bucketdistribution

--- lib/cv2-stack.ts
+++ lib/cv2-stack.ts
@@ -7,7 +7,6 @@ import {
 } from "aws-cdk-lib";
 import { Distribution } from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
-import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { Asset } from "aws-cdk-lib/aws-s3-assets";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
@@ -19,13 +18,13 @@ export class Cv2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // const bucket = new Bucket(this, "bucket", {
-    //   autoDeleteObjects: true,
-    //   removalPolicy: RemovalPolicy.DESTROY,
-    //   blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-    //   bucketName: "cv-repo",
-    //   enforceSSL: true,
-    // });
+    const bucket = new Bucket(this, "bucket", {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      bucketName: "cv-repo",
+      enforceSSL: true,
+    });
 
     const cvFileName = "cv.html";
 
@@ -41,14 +40,26 @@ export class Cv2Stack extends Stack {
         ],
       },
     });
-
+    new BucketDeployment(this, "bucket-deployment", {
+      destinationBucket: bucket,
+      sources: [
+        Source.asset(join(__dirname, "../"), {
+          bundling: {
+            image: DockerImage.fromBuild(join(__dirname, "../")),
+            command: [
+              "bash",
+              "-c",
+              'echo "heloo from contaiuner" >> /asset-input/cv.html && cp /asset-input/cv.html /asset-output/cv.html',
+            ],
+          },
+        }),
+      ],
+    });
     this.domainName = new Distribution(this, "distribution", {
       defaultBehavior: {
-        origin: new S3Origin(
-          Bucket.fromBucketName(this, "asset-bucket", asset.s3BucketName)
-        ),
+        origin: new S3Origin(bucket),
       },
-      defaultRootObject: asset.s3ObjectKey,
+      defaultRootObject: cvFileName,
     }).domainName;
 
     new CfnOutput(this, "domainName", {
