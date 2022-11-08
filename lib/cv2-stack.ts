622ee9487e7f961612d9f7122c448ae9aa105412
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-11-08 21:15:16 +0300

    try to use built image

--- lib/cv2-stack.ts
+++ lib/cv2-stack.ts
@@ -47,20 +47,20 @@ export class Cv2Stack extends Stack {
       value: image.imageUri,
     });
 
-    new BucketDeployment(this, "bucket-deployment", {
-      destinationBucket: bucket,
-      sources: [
-        Source.asset(join(__dirname, "../"), {
-          bundling: {
-            image: DockerImage.fromRegistry(image.imageUri),
-            command: [
-              "bash",
-              "-c",
-              'echo "heloo" >> /asset-input/cv.html && cp /asset-input/cv.html /asset-output/cv.html',
-            ],
-          },
-        }),
-      ],
-    });
+    // new BucketDeployment(this, "bucket-deployment", {
+    //   destinationBucket: bucket,
+    //   sources: [
+    //     Source.asset(join(__dirname, "../"), {
+    //       bundling: {
+    //         image: DockerImage.fromRegistry(image.imageUri),
+    //         command: [
+    //           "bash",
+    //           "-c",
+    //           'echo "heloo" >> /asset-input/cv.html && cp /asset-input/cv.html /asset-output/cv.html',
+    //         ],
+    //       },
+    //     }),
+    //   ],
+    // });
   }
 }
